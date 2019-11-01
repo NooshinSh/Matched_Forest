ec40e7ec2981408833621702d687dcb5bc3e55d2
NooshinSh <36524078+NooshinSh@users.noreply.github.com>
2019-10-31 18:03:06 -0700

    Update Matched_Forest.R

--- Matched_Forest.R
+++ Matched_Forest.R
@@ -1,3 +1,22 @@
+exp.imp <- function(imp,Match,Exp){
+    if (Match==0){
+      df1=data.frame()
+    }else{
+      df1=data.frame(imp[1:Match,])
+    }
+    
+    for (j in 1:Exp){
+      a=imp[(Match+2*j-1),]+imp[(Match+2*j),]+imp[(Match+2*Exp+j),]
+      df1=rbind(df1,a)
+    }
+  return(df1)
+}
+
+#pvalues
+pVal <- function(obs, null.dist) {
+  (sum(null.dist >= obs) + 1) / (length(null.dist) + 1)
+}
+
 ##################
 library(randomForest)
 library(ggplot2)
@@ -6,6 +25,7 @@ library(ggplot2)
 dir_main = paste('C:/.../',sep='')
 dir_input = paste(dir_main,'data/',sep='')
 dir_output = paste(dir_main,'output/',sep='')
+dir_input='C:/Users/Nooshin Shomalzadeh/Documents/Research/Matched Forest/Paper/Manuscript/code/'
 data_name='Childhood_Acute_Lymphoblastic_Leukemia_Data'
 ############################################################
 df<-read.table(paste(dir_input,data_name,'.csv',sep=''),header = TRUE)
@@ -35,33 +55,81 @@ if (is.null(Match_indx)){
 ###########################################################
 #2. RF with matched variable importance
 Run=10
-ntree=5
+p.iter=100
+ntree=50000
 M=dim(df_transformed)[2]-1
 mtry=floor(sqrt(M))
-sampsize=dim(df_transformed)[1]
+N=dim(df_transformed)[1]
 maxnodes=NULL
 
 #2.1. RF
-imp<-matrix(nrow=M,ncol=Run)
+imp_null<-matrix(0,nrow=M,ncol=p.iter)
+imp_obs<-matrix(0,nrow=M,ncol=Run)
+impE_null<-matrix(0,nrow=Match+3*Exp,ncol=p.iter)
+impE_obs<-matrix(0,nrow=Match+3*Exp,ncol=Run)
+pval.mat <- matrix(0,nrow=Match+3*Exp,ncol=1)
+#observed scores
+X=df_transformed[,2:(ncol(df_transformed))]
+y=as.factor(df_transformed[,1])
 for (i in c(1:Run)){
   print(paste('Run=',i))
-  MF <- randomForest(df_transformed[,2:(ncol(df_transformed))], y =  as.factor(df_transformed[,1]), 
-                     mtry=mtry, sampsize=sampsize,
+  MF <- randomForest(x=X, y = y, 
+                     mtry=mtry, sampsize=N,
                      replace = TRUE, importance=TRUE, ntree = ntree, 
                      maxnodes = maxnodes, keep.forest = TRUE)
-  imp[,i]<-importance(MF,type=1)
+  imp_obs[,i]<-importance(MF,type=1)
 }
+#null distribution for scores
+
+for (j in c(1:p.iter)){
+  print(paste('permute = ',j,sep=''))
+  XP=as.data.frame(matrix(0,nrow=N,ncol=M))
+  if(Match>0){
+    XP[,(1:Match)]=X[,c(1:Match)]
+  }
+  for(k in c(1:2)){
+    cols=c(Match+2*k-1,Match+2*k)
+    dat=X[1:(N/2),cols]
+    datp<-apply(dat,1, function(z){
+      sample(z,2,replace=FALSE)
+    })
+    #d*
+    XP[1:(N/2),(Match+2*Exp+k)]=datp[1,]-datp[2,]
+    XP[(N/2+1):N,(Match+2*Exp+k)]=datp[2,]-datp[1,]
+    #case*, control*
+    XP[1:(N/2),cols]<-t(datp)
+    XP[(N/2+1):N,cols]<-cbind(datp[2,],datp[1,])
+  }
+  MFP <- randomForest(x=XP, y=y, 
+                     mtry=mtry, sampsize=N,
+                     replace = TRUE, importance=TRUE, ntree = ntree, 
+                     maxnodes = maxnodes, keep.forest = TRUE)
+  imp_null[,j]<-importance(MFP,type=1)
+}
+
+
+
+
 
 #2.2. Matched variable importance
+impE_null<-exp.imp(imp_null,Match,Exp)
+impE_obs<-exp.imp(imp_obs,Match,Exp)
 
-#2.2.1. Matching Variables
-if (is.null(Match_indx)==FALSE){
-  impM<-imp[1:Match,]
-}
+#2.3. Variable selection
 
-#2.2.2. Exposure variables
-imp_case<-imp[c((Match+1):(Match+Exp)),]
-imp_control<-imp[c((Match+1+Exp):(Match+2*Exp)),]
-imp_d<-imp[c((Match+2*Exp+1):(Match+3*Exp)),]
+impExp.obs.mean.mat=apply(impE_obs,1,mean)
 
-impE<-imp_case+imp_control+imp_d
+pval_exp = sapply(1:(Match+Exp),function(x){
+  obs=impExp.obs.mean.mat[x]
+  null.dist=impE_null[x,]
+  pVal(obs, null.dist)
+})
+
+pval.mat=as.matrix(pval_exp)
+
+#save output
+row.names(impE_obs)=row.names(pval.mat)<-colnames(df)[c(Match_indx,Exp_indx)]
+colnames(impE_obs)<-seq(1,Run,1)
+Write.csv(impE_obs,paste(dir_output,'Variable_MFI_score.csv',sep=''))
+Write.csv(pval.mat,paste(dir_output,'Variable_pvalue.csv',sep=''))
+
